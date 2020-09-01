436d898f5ecb2f430ecd8d6e7166c60da210eeee
Madeline Uribes <madeline.uribes@gmail.com>
2020-09-01 12:16:31 -0400

    Scaling circle's radius

--- today.js
+++ today.js
@@ -27,6 +27,10 @@ todaySvg
 
 const monthSvg = d3.select("svg.month")
 
+const radiusScale = d3.scaleSqrt()
+  .domain([0, 30000])
+  .range([0, 50])
+
 monthSvg
   .selectAll("circle")
   .data(monthData)
@@ -34,4 +38,4 @@ monthSvg
   .append("circle")
   .attr("cx", (d, i) => { return (i % 7) * 120 + 60 })
   .attr("cy", (d, i) => { return Math.floor(i / 7) * 100 + 60 })
-  .attr("r", (d, i) => { return 5 })
+  .attr("r", (d, i) => { return radiusScale(d) })

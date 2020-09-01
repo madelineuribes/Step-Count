e99a136a0836fae2864a8a3cdb77584eba379914
Madeline Uribes <madeline.uribes@gmail.com>
2020-09-01 08:28:18 -0400

    Introducing Scales

--- today.js
+++ today.js
@@ -1,13 +1,17 @@
 
 const todaySvg = d3.select("svg")
 
+const barScale = d3.scaleLinear()
+  .domain([0, 2000])
+  .range([1, 112])
+
 todaySvg
   .selectAll("rect")
   .data(todayData)
   .enter()
   .append("rect")
   .attr("x", (d, i) => { return i * 36 })
-  .attr("y", (d, i) => { return 112 - d })
+  .attr("y", (d, i) => { return 112 - barScale(d) })
   .attr("width", 24)
-  .attr("height", (d, i) => { return d })
+  .attr("height", (d, i) => { return barScale(d) })
 
